9f29139f40a346e7046ebc82d2a0b2c11fcba460
daphne remali <remalidaphne@gmail.com>
2017-10-26 16:45:39 +0200

    add ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
 import { ReadingListComponent } from './reading-list/reading-list.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleComponent } from './article/article.component';
+
+import { ReadingListReducer } from './store/reading-list.reducer';
+import { EditArticleComponent } from './articles/edit-article/edit-article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
     ReadingListComponent,
+    ArticlesComponent,
+    ArticleComponent,
+    EditArticleComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
-
-
+    StoreModule.forRoot({ readingList: ReadingListReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
