a13f137482e40f5ecf63132e59b51d6b4786711a
daphne remali <remalidaphne@gmail.com>
2017-11-30 00:00:35 +0100

    add HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent }   from './home/home.component';
 
 import { ArticlesComponent }   from './articles/articles.component';
 import { ArticleComponent }   from './article/article.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',  component: HomeComponent },
   { path: 'articles',  component: ArticlesComponent },
   { path: 'articles/:_id', component: ArticleComponent },
 ];
